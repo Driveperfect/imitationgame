d31824952b5f51b29560f9f454a1431bf788ff84
Poenai <vinod@poenai.nl>
2015-01-08 21:27:47 +0100

    Menu items

---
+++ menu.php
@@ -0,0 +1,46 @@
+						<ul class="main-navigation-menu">
+							<li class="active open">
+								<a href="index.html"><i class="fa fa-home"></i> <span class="title"> Dashboard </span><span class="label label-default pull-right ">LABEL</span> </a>
+							</li>
+							<li> <!-- 3 level menu voor de registratie van KM en Uren -->
+								<a href="javascript:;" class="active">
+									<i class="fa fa-folder"></i> <span class="title"> Registratie </span> <i class="icon-arrow"></i>
+								</a>
+								<ul class="sub-menu">
+									<li>
+										<a href="javascript:;">
+											Kilometer <i class="icon-arrow"></i>
+										</a>
+										<ul class="sub-menu">
+											<li>
+												<a href="kilometerregistratie.php">
+													Toevoegen
+												</a>
+											</li>
+											<li>
+												<a href="#">
+													Statistieken
+												</a>
+											</li>
+										</ul>
+									</li>
+									<li>
+										<a href="javascript:;">
+											Uren <i class="icon-arrow"></i>
+										</a>
+										<ul class="sub-menu">
+											<li>
+												<a href="#">
+													Schrijven
+												</a>
+											</li>
+											<li>
+												<a href="#">
+													Rapportage
+												</a>
+											</li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+						</ul>
